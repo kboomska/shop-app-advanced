506e52b21301b5b3a11bc62c329dec58e5258271
Aleksey Kuzmin <kboomska@gmail.com>
2023-06-07 20:32:33 +0300

    ShopApp: App initialization

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'package:shop_app/ui/widgets/app/shop_app.dart';
+
+void main() {
+  const app = ShopApp();
+  runApp(app);
+}
---
+++ lib/ui/widgets/app/shop_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ShopApp extends StatelessWidget {
+  const ShopApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Shop App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
